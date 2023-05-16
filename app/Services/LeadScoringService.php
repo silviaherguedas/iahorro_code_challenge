372e6ccf2be179a3365a8a87fed5830fc3582c7b
Silvia M. Herguedas <silviaherguedas@gmail.com>
2023-05-16 20:34:51 +0200

    Update LeadScoringService to include comments and implementation of LeadRepository

--- app/Services/LeadScoringService.php
+++ app/Services/LeadScoringService.php
@@ -4,13 +4,24 @@ namespace App\Services;
 
 use App\Repositories\LeadRepository;
 
-
+/**
+ * Service dedicated to calculating the score value for a given Lead.
+ *
+ * In this case and because we do not have the necessary business logic to be able to
+ * perform a proper calculation, the tool will automatically generate a random number,
+ * whose value is applied numerically between 0 (the lowest) and 100 (the highest score).
+ * The higher the score, the more interesting the customer or lead is, as it means that
+ * he or she is more likely to make a purchase.
+ */
 class LeadScoringService
 {
     const MIN = 0;
     const MAX = 100;
     const ROUND = 2;
 
+    /**
+     * @var LeadRepository
+     */
     protected $leadRepository;
 
     public function __construct(LeadRepository $leadRepository)
